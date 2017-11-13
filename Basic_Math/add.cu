7dc4c6e3062109b1939c2624f60cf24c63b49e23
Parth Kachchhi <themoonraker13@gmail.com>
2017-11-13 13:18:24 -0500

    Added basic cuda programs

---
+++ Basic_Math/add.cu
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <host_defines.h>
+#include <curand_mtgp32_kernel.h>
+
+__global__ void add(int *d_out, int *d_in) {
+    int idx = threadIdx.x;
+    int f = d_in[idx];
+    d_out[idx] = f + f;
+}
+
+int main(int argc, char **argv) {
+    const int ARRAY_SIZE = 64;
+    const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
+
+    // generate the input array on the host
+    int h_in[ARRAY_SIZE];
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        h_in[i] = int(i);
+    }
+    int h_out[ARRAY_SIZE];
+
+    // declare GPU memory pointers
+    int *d_in;
+    int *d_out;
+
+    // allocate GPU memory
+    cudaMalloc((void **) &d_in, ARRAY_BYTES);
+    cudaMalloc((void **) &d_out, ARRAY_BYTES);
+
+    // transfer the array to GPU
+    cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+    // launch the kernel
+    add<<< 1, ARRAY_SIZE>>>(d_out, d_in);
+
+    // copy back the result array to the CPU
+    cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+
+    // print out the resulting array
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        printf("%i", h_out[i]);
+	printf(((i % 4) != 3) ? "\t" : "\n");
+    }
+
+    // free GPU memory allocation
+    cudaFree(d_in);
+    cudaFree(d_out);
+
+    return 0;
+}
